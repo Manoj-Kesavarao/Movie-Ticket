aeb0e13c51476543f789a06b353982ea9b43d5f3
Manoj-Kesavarao <118155719+Manoj-Kesavarao@users.noreply.github.com>
2022-11-14 05:45:44 +0000

    code

---
+++ movie.py
@@ -0,0 +1,272 @@
+print("Welcome to BookMyShow")
+city="Coimbatore"
+a=input("Enter your city:")
+if(city==a):
+    print("1-Poniyan Selvan\n2-Varisu\n3-Thunivu\n4-Vaadi Vaasal")
+    b=int(input("Enter your choice:"))
+    if(b==1):
+        print("1-9.45am\n2-2.30pm\n3-10.30pm")
+        c=int(input("Select your time:"))
+        if(c==1):
+            print("1-balcony\n2-comfort\n3-avg")
+            d=int(input("Enter your choice:"))
+            if(d==1):
+                balcony=190
+                gst=30
+                e=int(input("Enter your seats:"))
+                sum=balcony*e+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+            elif(d==2):
+                comfort=150
+                gst=30
+                f=int(input("Enter your seats:"))
+                sum=comfort*f+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+            elif(d==3):
+                avg=100
+                gst=30
+                g=int(input("Enter your seats:"))
+                sum=avg*g+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+        elif(c==2):
+            print("1-balcony\n2-comfort\n3-avg")
+            h=int(input("Enter your choice:"))
+            if(h==1):
+                balcony=190
+                gst=30
+                i=int(input("Enter your seats:"))
+                sum=balcony*i+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+            elif(h==2):
+                comfort=150
+                gst=30
+                j=int(input("Enter your seats:"))
+                sum=comfort*j+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+            elif(h==3):
+                avg=100
+                gst=30
+                k=int(input("Enter your seats:"))
+                sum=avg*k+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+        elif(c==3):
+            print("1-balcony\n2-comfort\n3-avg")
+            l=int(input("Enter your choice:"))
+            if(l==1):
+                balcony=190
+                gst=30
+                m=int(input("Enter your seats:"))
+                sum=balcony*m+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT =",sum)
+            elif(l==2):
+                comfort=150
+                gst=30
+                n=int(input("Enter your seats:"))
+                sum=comfort*n+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+            elif(l==3):
+                avg=100
+                gst=30
+                o=int(input("Enter your seats:"))
+                sum=avg*o+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT",sum)
+    if(b==2):
+        print("1-9.45am\n2-2.30pm\n3-10.30pm")
+        p=int(input("Select your time"))
+        if(p==1):
+            print("1-balcony\n2-comfort\n3-avg")
+            q=int(input("Enter your choice:"))
+            if(q==1):
+                balcony=190
+                gst=30
+                r=int(input("Enter your seats:"))
+                sum=balcony*r+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(q==2):
+                comfort=150
+                gst=30
+                s=int(input("Enter your seats:"))
+                sum=comfort*s+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(q==3):
+                avg=100
+                gst=30
+                t=int(input("Enter your seats:"))
+                sum=avg*t+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(p==2):
+            print("1-balcony\n2-comfort\n3-avg")
+            u=int(input("Enter your choice:"))
+            if(u==1):
+                balcony=190
+                gst=30
+                v=int(input("Enter your seats:"))
+                sum=balcony*v+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(u==2):
+                comfort=150
+                gst=30
+                w=int(input("Enter your seats:"))
+                sum=comfort*w+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(u==3):
+                avg=100
+                gst=30
+                x=int(input("Enter your seats:"))
+                sum=avg*x+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(p==3):
+            print("1-balcony\n2-comfort\n3-avg")
+            y=int(input("Enter your choice:"))
+            if(y==1):
+                balcony=190
+                gst=30
+                z=int(input("Enter your seats:"))
+                sum=balcony*z+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT =", sum)
+            elif(y==2):
+                comfort=150
+                gst=30
+                A=int(input("Enter your seats:"))
+                sum=comfort*A+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif (y==3):
+                avg=100
+                gst=30
+                B=int(input("Enter your seats:"))
+                sum=avg*B+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+    if (b==3):
+        print("1-9.45am\n2-2.30pm\n3-10.30pm")
+        C=int(input("Select your time:"))
+        if(C==1):
+            print("1-balcony\n2-comfort\n3-avg")
+            D=int(input("Enter your choice:"))
+            if(D==1):
+                balcony=190
+                gst=30
+                E=int(input("Enter your seats:"))
+                sum=balcony*E+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(D==2):
+                comfort=150
+                gst=30
+                F=int(input("Enter your seats:"))
+                sum=comfort*F+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(D==3):
+                lowe=100
+                gst=30
+                G=int(input("Enter your seats:"))
+                sum=avg*G+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(C==2):
+            print("1-balcony\n2-comfort\n3-avg")
+            H=int(input("Enter your choice:"))
+            if(H==1):
+                balcony=190
+                gst=30
+                I=int(input("Enter your seats:"))
+                sum=balcony*I+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(H==2):
+                comfort=150
+                gst=30
+                J=int(input("Enter your seats:"))
+                sum=comfort*J+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(H==3):
+                avg=100
+                gst=30
+                K=int(input("Enter your seats:"))
+                sum=avg*K+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(C==3):
+            print("1-balcony\n2-comfort\n3-avg")
+            L=int(input("Enter your choice:"))
+            if(L==1):
+                balcony=190
+                gst=30
+                M=int(input("Enter your seats:"))
+                sum=balcony*M+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT =", sum)
+            elif(L==2):
+                averag=150
+                gst=30
+                N=int(input("Enter your seats:"))
+                sum=comfort*N+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(L==3):
+                avg=100
+                gst=30
+                O=int(input("Enter your seats:"))
+                sum=avg*O+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+    if(b==4):
+        print("1-9.45am\n2-2.30pm\n3-10.30pm")
+        P=int(input("Select your time:"))
+        if(P==1):
+            print("1-balcony\n2-comfort\n3-avg")
+            Q=int(input("Enter your choice:"))
+            if(Q==1):
+                balcony=190
+                gst=30
+                R=int(input("Enter your seats:"))
+                sum=balcony*R+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(Q==2):
+                comfort=150
+                gst=30
+                S=int(input("Enter your seats:"))
+                sum=comfort*S+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(Q==3):
+                avg=100
+                gst=30
+                T=int(input("Enter your seats:"))
+                sum=avg*T+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(P==2):
+            print("1-balcony\n2-comfort\n3-avg")
+            U=int(input("Enter your choice:"))
+            if(U==1):
+                balcony=190
+                gst=30
+                V=int(input("Enter your seats:"))
+                sum=balcony*V+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(U==2):
+                comfort150
+                gst=30
+                W=int(input("Enter your seats:"))
+                sum=comfort*W+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(U==3):
+                avg=100
+                gst=30
+                X=int(input("Enter your seats:"))
+                sum=avg*X+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+        elif(P==3):
+            print("1-balcony\n2-comfort\n3-avg")
+            Y=int(input("Enter your choice:"))
+            if(Y==1):
+                balcony=190
+                gst=30
+                Z=int(input("Enter your seats:"))
+                sum=balcony*Z+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT =", sum)
+            elif(Y==2):
+                comfort=150
+                gst=30
+                AB=int(input("Enter your seats:"))
+                sum=comfort*AB+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+            elif(Y==3):
+                avg=100
+                gst=30
+                CD=int(input("Enter your seats:"))
+                sum=avg*CD+gst
+                print("TICKETS BOOKED SUCCESSFULLY\nTOTAL AMOUNT", sum)
+else:
+    print("Please enter the correct city")
